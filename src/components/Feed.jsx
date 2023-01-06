dc5da99de59d2b337336a1e5c04e6447bfca9185
mariosknl <mariosknl@outlook.com>
2023-01-06 18:47:47 +0200

    videocard component created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+	const [selectedCategory, setSelectedCategory] = useState("New");
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+			setVideos(data.items)
+		);
+	}, [selectedCategory]);
+
 	return (
 		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
 			<Box
@@ -12,7 +23,10 @@ const Feed = () => {
 					px: { sx: 0, md: 2 },
 				}}
 			>
-				<Sidebar />
+				<Sidebar
+					selectedCategory={selectedCategory}
+					setSelectedCategory={setSelectedCategory}
+				/>
 				<Typography
 					className="copyright"
 					variant="body2"
@@ -20,6 +34,21 @@ const Feed = () => {
 				>
 					Copyright @2022 Marios Knl
 				</Typography>
+			</Box>
+
+			<Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+				<Typography
+					variant="h4"
+					fontWeight="bold"
+					mb={2}
+					sx={{
+						color: "white",
+					}}
+				>
+					{selectedCategory} <span style={{ color: "#Fc1503" }}>videos</span>
+				</Typography>
+
+				<Videos videos={videos} />
 			</Box>
 		</Stack>
 	);
